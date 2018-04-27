2a1bc7ce37e89a81c3051bc30a23db4179743954
Demchenko Evgeny <demchenko@inf.ai>
2018-04-27 20:25:18 +0400

    add dependencies to package.json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const {ErrorParser} = require('./lib/ErrorsParser');
-const {Express} = require('./lib/Express');
+const {Express, bodyParser, cookieParser, express} = require('./lib/Express');
 const {RestRouter, errorsParser} = require('./lib/RestRouter');
 
 
-module.exports = {Express, RestRouter, ErrorParser, errorsParser };
+module.exports = {Express, RestRouter, ErrorParser, errorsParser, bodyParser, cookieParser, express };
