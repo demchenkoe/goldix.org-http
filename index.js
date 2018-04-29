b29b5236a31647a02b26f9e7c95bbbcbe71691f8
Demchenko Evgeny <demchenko@inf.ai>
2018-04-29 18:22:40 +0400

    more changes for Rest

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const {ErrorParser} = require('./lib/ErrorsParser');
 const {Express, bodyParser, cookieParser, express} = require('./lib/Express');
-const {RestRouter, errorsParser} = require('./lib/RestRouter');
+const {Rest} = require('./lib/Rest');
 
 
-module.exports = {Express, RestRouter, ErrorParser, errorsParser, bodyParser, cookieParser, express };
+module.exports = {Express, Rest, bodyParser, cookieParser, express };
