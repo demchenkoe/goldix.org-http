82e5b3326dffe646ad7288f836a4b45d3b978ae6
Demchenko Evgeny <demchenko@inf.ai>
2018-07-18 21:41:31 +0400

    generator for openapi json document from meta data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const {Express, bodyParser, cookieParser, express} = require('./lib/Express');
 const {Rest} = require('./lib/Rest');
+const {OpenApi} = require('./lib/OpenApi');
 
 
-module.exports = {Express, Rest, bodyParser, cookieParser, express };
+module.exports = {Express, Rest, OpenApi, bodyParser, cookieParser, express };
